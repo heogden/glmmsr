02a70f28017b2621ec11ce2660617776277ddb41
Helen Ogden <heogden12@gmail.com>
2015-03-04 16:00:35 +0000

    copy structure for glmerSR from glmer

---
+++ R/fit.R
@@ -0,0 +1,7 @@
+glmerSR <- function(formula, data = NULL, family = gaussian,
+                    control = glmerControl(), start = NULL, verbose = 0L,
+                    nAGQ = 1L, k = 0L, subset, weights, na.action, offset,
+                    contrasts = NULL, mustart, etastart,
+                    devFunOnly = FALSE, ...) {
+
+}
