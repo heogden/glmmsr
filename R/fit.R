8d8b90f14762049a315386de15d648ed5f870232
Helen Ogden <heogden12@gmail.com>
2015-03-04 16:09:34 +0000

    add glmerSR documentation

--- R/fit.R
+++ R/fit.R
@@ -1,3 +1,9 @@
+#' Fit a GLMM
+#'
+#' @param k integer scalar - the level of approximation used in the sequential
+#'  reduction approximation to the likelihood.
+#' @inheritParams lme4::glmer
+#' @export
 glmerSR <- function(formula, data = NULL, family = gaussian,
                     control = glmerControl(), start = NULL, verbose = 0L,
                     nAGQ = 1L, k = 0L, subset, weights, na.action, offset,
