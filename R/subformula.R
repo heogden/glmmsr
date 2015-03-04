72bb2cbef8d4e555797029a85849c303c59f01d0
Helen Ogden <heogden12@gmail.com>
2015-03-04 16:56:05 +0000

    add function to parse subformula interface

---
+++ R/subformula.R
@@ -0,0 +1,10 @@
+#' Parse a formula (and possibly subformulas)
+#'
+#' @inheritParams glmerSR
+#' @export
+glFormulaSub <- function (formula, data = NULL, family = gaussian, subset,
+                          weights, na.action, offset, contrasts = NULL, mustart,
+                          etastart, control = glmerControl(), ...)
+{
+
+}
