59631d3f266f198d8746c162e3756d103707ee27
Helen Ogden <heogden12@gmail.com>
2015-03-04 15:46:47 +0000

    add package-level documentation

---
+++ R/glmmsr.R
@@ -0,0 +1,25 @@
+#' glmmsr: fit GLMMs with the sequential reduction approximation
+#'
+#' The glmmsr package provides functions to do inference on generalized linear
+#' mixed models, using the sequential reduction approximation to the likelihood.
+#' The interface and output are designed to look similar to lme4.
+#'
+#' The \code{lme4} package uses the Laplace approximation to the likelihood,
+#' except in the special case of two-level models, where adapative Gaussian
+#' quadrature may be used. In some situations, this Laplace approximation may be
+#' poor quality, and the sequential reduction approximation may be used to find
+#' an accurate approximation to the likelihood.
+#'
+#' The main function of the glmmsr package is \code{\link{glmerSR}}, which is
+#' used to fit the GLMM. Its interface is a generalization of
+#' \code{\link[lme4]{glmer}}, to allow a larger class of models, including
+#' structured pairwise comparison models. This interface is described in
+#' \code{\link{subform}}.
+#'
+#' @references Helen E. Ogden (2015). A sequential reduction method for
+#'   inference in generalized linear mixed models. Electronic Journal of
+#'   Statistics 9: 135-152. doi:
+#'   \href{http://dx.doi.org/10.1214/15-EJS991}{10.1214/15-EJS991}
+#' @docType package
+#' @name glmmsr
+NULL
