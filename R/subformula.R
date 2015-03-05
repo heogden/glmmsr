dbe84340e6464df71821c8e135d751b7bfa69da6
Helen Ogden <heogden12@gmail.com>
2015-03-05 08:40:08 +0000

    pass to glFormula if no Sub

--- R/subformula.R
+++ R/subformula.R
@@ -6,5 +6,18 @@ glFormulaSub <- function (formula, data = NULL, family = gaussian, subset,
                           weights, na.action, offset, contrasts = NULL, mustart,
                           etastart, control = glmerControl(), ...)
 {
+  #detect whether formula contains subformulas
+  tf <- terms(formula, specials = "Sub")
+  stf <- attr(tf, "specials")
+  if(length(stf$Sub) == 0L) {
+    #if there are no subformulas, can just use glFormula
+
+    mc <- match.call()
+    mc[[1]] <- quote(lme4::glFormula)
+    return(eval(mc, parent.frame()))
+  } else{
+    # there are subformulas
+
+  }
 
 }
---
+++ tests/testthat/test_subformula.R
@@ -0,0 +1,25 @@
+library(glmmsr)
+
+set.seed(1)
+player <- 1:10
+player1 <- factor(rep(2:10, 10), levels = player)
+player2 <- factor(rep(1:9, 10), levels = player)
+
+x <- rnorm(length(player))
+u0 <- rnorm(length(player))
+
+beta0 <- 1
+ability0 <- beta0*x + u0
+p0 <- pnorm(ability0[player1] - ability0[player2])
+y <- rbinom(length(p0), 1, p0)
+
+formula <- y ~ 0 + Sub(ability[player1] - ability[player2])
+subform <- ability ~ 0 + x + (1 | player)
+data <- list(x=x, player1 = player1, player2 = player2, player = player)
+
+test_that("passes to glFormula if no Sub() terms", {
+    form <- y ~ 0 + (1 | player1) + (1 | player2)
+    modfr1 <- glFormulaSub(form, data = data, family = binomial)
+    modfr2 <- lme4::glFormula(form, data = data, family = binomial)
+    expect_equal(modfr1, modfr2)
+})
