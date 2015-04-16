11e36ade261802937de8f55b0bc71efd92959799
Helen Ogden <heogden12@gmail.com>
2015-04-16 10:04:25 +0100

    fix testing of three-level model

--- tests/testthat/test_splitframe.R
+++ tests/testthat/test_splitframe.R
@@ -2,35 +2,44 @@ library(glmmsr)
 context("Split up model frame")
 
 set.seed(1)
-y <- rbinom(10, 1, 0.5)
-x <- rbinom(10, 1, 0.5)
-cluster <- rep(1:5, each = 2)
+
+n_clusters <- 5
+n_in_cluster <- 6
+n <- n_clusters*n_in_cluster
+
+y <- rbinom(n, 1, 0.5)
+x <- rbinom(n, 1, 0.5)
+
+cluster <- rep(1:n_clusters, each = n_in_cluster)
 data = list(y = y, x = x, cluster = cluster)
 modfr <- parse_formula(y ~ x + (1 | cluster), data = data,
                        family = binomial, off = NULL)
+q <- nrow(modfr$reTrms$Zt)
 
 # for a three-level model
-big_cluster <- rep(1:2, each = 5)
-data_3 = list(y = y, x = x, cluster = cluster, big_cluster = big_cluster)
-modfr_3 <- parse_formula(y ~ x + (1 | cluster) + (1 | big_cluster),
+n_in_subcluster <- 2
+n_subclusters <- n_clusters * n_in_cluster/n_in_subcluster
+subcluster <- rep(1:n_subclusters, each = n_in_subcluster)
+data_3 = list(y = y, x = x, cluster = cluster, subcluster = subcluster)
+modfr_3 <- parse_formula(y ~ x + (1 | cluster) + (1 | subcluster),
                          data = data_3,
                          family = binomial, off = NULL)
+q_3 <- nrow(modfr_3$reTrms$Zt)
 
 test_that("find posterior dependence graph correctly", {
   act <- find_active(modfr)
-  q <- nrow(modfr$reTrms$Zt)
   G <- find_pdg(act, q)
   expect_equal(length(igraph::V(G)), q)
   expect_equal(length(igraph::E(G)), 0)
   act_3 <- find_active(modfr_3)
-  q_3 <- nrow(modfr_3$reTrms$Zt)
   G_3 <- find_pdg(act_3, q_3)
   expect_equal(length(igraph::V(G_3)), q_3)
+  expect_equal(length(igraph::E(G_3)), n_clusters*n_in_cluster/n_in_subcluster)
 })
 
 test_that("reordering model frame keeps devfun unchanged", {
   devfun <- lme4::mkGlmerDevfun(modfr$fr, modfr$X, modfr$reTrms, modfr$family)
-  elim_order <- c(2:5, 1)
+  elim_order <- c(2:q, 1)
   modfr2 <- reorder_modfr(modfr, elim_order)
   devfun2 <- lme4::mkGlmerDevfun(modfr2$fr, modfr2$X, modfr2$reTrms,
                                  modfr2$family)
