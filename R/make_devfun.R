cdba5ff4887671a77e7183c01ef2bb290e80ebf2
Helen Ogden <heogden12@gmail.com>
2015-06-18 09:45:38 +0100

    protect from change to mkGlmerDevfun interface

--- R/make_devfun.R
+++ R/make_devfun.R
@@ -6,7 +6,9 @@ mkGlmerDevfunSR <- function(fr, X, reTrms, family, nAGQ = 1L, k = 0L,
                             verbose = 0L, control = glmmsrControl(), ...)
 {
   if(k == 0L) {
-    devfun <- mkGlmerDevfun(fr, X, reTrms, family, nAGQ, verbose, control, ...)
+    devfun <- mkGlmerDevfun(fr = fr, X = X, reTrms = reTrms, family = family,
+                            nAGQ = nAGQ, verbose = verbose,
+                            control = control, ...)
     devfun <- updateGlmerDevfun(devfun, reTrms, nAGQ)
   } else {
     modfr <- list(fr = fr, X = X, reTrms = reTrms, family = family)
