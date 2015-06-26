cedf9eb46ec5b397e36805909a31a24ecb27900a
Helen Ogden <heogden12@gmail.com>
2015-06-26 11:05:13 +0100

    update two- and three-level examples

--- inst/examples/three_level.R
+++ inst/examples/three_level.R
@@ -1,15 +1,26 @@
 set.seed(1)
 
-n_clusters <- 5
-n_in_cluster <- 6
-n <- n_clusters * n_in_cluster
-
-y <- rbinom(n, 1, 0.5)
-x <- rbinom(n, 1, 0.5)
-
+n_clusters <- 25
+n_subclusters_in_cluster <- 2
 n_in_subcluster <- 2
-n_subclusters <- n_clusters * n_in_cluster/n_in_subcluster
+n <- n_clusters * n_subclusters_in_cluster * n_in_subcluster
+
+n_subclusters <- n_clusters * n_in_subcluster
 subcluster <- rep(1:n_subclusters, each = n_in_subcluster)
+
+n_in_cluster <- n_in_subcluster * n_subcluster_in_cluster
+cluster <- rep(1:n_clusters, each = n_in_cluster)
+
+# simulate some data
+x <- rbinom(n, 1, 0.5)
+sigma0 <- c(0.5, 1)
+beta0 <- -0.5
+u0 <- rnorm(n_subclusters)
+v0 <- rnorm(n_clusters)
+eta <- beta0 * x + sigma0[1] * u0[subcluster] + sigma0[2] * v0[cluster]
+p <- exp(eta) / (1 + exp(eta))
+y <- rbinom(p)
+
 data = list(y = y, x = x, cluster = cluster, subcluster = subcluster)
 
 devfun_approx <- glmerSR(y ~ x + (1 | cluster) + (1 | subcluster),
@@ -20,5 +31,5 @@ devfun_10_4 <- glmerSR(y ~ x + (1 | cluster) + (1 | subcluster),
                        nAGQ = 10, k = 4,
                        devFunOnly = TRUE)
 
-devfun_approx(c(0.5, 1.5, -0.5))
-devfun_10_4(c(0.5, 1.5, -0.5))
+devfun_approx(c(0.5, 1, -0.5))
+devfun_10_4(c(0.5, 1, -0.5))
--- inst/examples/two_level.R
+++ inst/examples/two_level.R
@@ -1,13 +1,19 @@
 set.seed(1)
 
-n_clusters <- 5
-n_in_cluster <- 6
+n_clusters <- 50
+n_in_cluster <- 2
 n <- n_clusters*n_in_cluster
-
-y <- rbinom(n, 1, 0.5)
-x <- rbinom(n, 1, 0.5)
-
 cluster <- rep(1:n_clusters, each = n_in_cluster)
+
+# simulate some data
+x <- rbinom(n, 1, 0.5)
+sigma0 <- 1
+beta0 <- -0.5
+u0 <- rnorm(n_clusters)
+eta <- beta0 * x + sigma0 * u0[cluster]
+p <- exp(eta) / (1 + exp(eta))
+y <- rbinom(p)
+
 data = list(y = y, x = x, cluster = cluster)
 
 devfun_approx <- glmerSR(y ~ x + (1 | cluster), data = data, family = binomial,
@@ -16,6 +22,7 @@ devfun_10_lme4 <- glmerSR(y ~ x + (1 | cluster), data = data, family = binomial,
                           nAGQ = 10, k = 0)
 devfun_10_sr <- glmerSR(y ~ x + (1 | cluster), data = data, family = binomial,
                         nAGQ = 10, k = 0)
+
 devfun_approx(c(1, -0.5))
 devfun_10_lme4(c(1, -0.5))
 devfun_10_sr(c(1, -0.5))
