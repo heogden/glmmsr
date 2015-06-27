5f52b462de58b169653e29cc4c0c89cb723a0c0e
Helen Ogden <heogden12@gmail.com>
2015-06-27 19:55:16 +0100

    remove old code from flatlizards example

--- inst/examples/flatlizards.R
+++ inst/examples/flatlizards.R
@@ -43,40 +43,12 @@ devfun_10_2 <- glmerSR(y ~ 0 + Sub(ability[winner] - ability[loser]),
                         devFunOnly = TRUE)
 
 devfun_10_2(0.5)
-# compare with the same model fit with BradleyTerry2
+
+# compare with model fit with BradleyTerry2
 
 mod_BTm <- BTm(y, winner, loser, ~ throat.PC1[..] + throat.PC3[..] +
               head.length[..] + SVL[..] + (1|..),
               family = binomial(link = "probit"), data = flatlizards)
 summary(mod_BTm)
 
-# create and split model frame
-modfr <- glFormulaSub(y ~ 0 + Sub(ability[winner] - ability[loser]),
-                      ability[liz] ~ 0 + liz96[liz] + liz99[liz] + throat.PC1[liz] +
-                        throat.PC3[liz] + head.length[liz] + SVL[liz] + (1 | liz),
-                      data = lizdat, family = binomial(link = "probit"))
-lmodfr <- split_modfr(modfr)
-q <- nrow(modfr$reTrms$Zt)
-#save_lmodfrs(lmodfr, q, file = "lmodfr_lizards.txt")
 
-# extract normal approximation
-devfun_lme4 <- lme4::mkGlmerDevfun(modfr$fr, modfr$X, modfr$reTrms, modfr$family,
-                    control = glmerControl(check.response.not.const = "ignore"))
-devfun_lme4 <- updateGlmerDevfun(devfun_lme4, modfr$reTrms)
-
-theta <- 0.5
-beta <- rep(0, 6)
--devfun_lme4(c(theta, beta))/2
-
-PR <- get("pp", environment(devfun_lme4))
-L_tot <- expand(PR$L())
-L <- L_tot$L
-P <- L_tot$P
-t(P)%*%L%*%t(L)%*%P
-
-Sigma_inv <- t(P)%*%tcrossprod(L)%*%P
-mu <- PR$delu
-
-#save_normal(mu, Sigma_inv, file = "normal_lizards.txt")
-
-Sigma <- solve(Sigma_inv)
