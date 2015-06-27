fbf4c96f5028257332bfc94c86711281263c408b
Helen Ogden <heogden12@gmail.com>
2015-06-27 16:32:56 +0100

    fix extracting theta and beta

--- R/make_devfun.R
+++ R/make_devfun.R
@@ -8,34 +8,47 @@ mkGlmerDevfunSR <- function(fr, X, reTrms, family, nAGQ = 1L, k = 0L,
   devfun_lme4 <- mkGlmerDevfun(fr = fr, X = X, reTrms = reTrms, family = family,
                           nAGQ = nAGQ, verbose = verbose,
                           control = control, ...)
-  devfun_lme4 <- updateGlmerDevfun(devfun_lme4, reTrms, nAGQ)
+  devfun_lme4 <- updateGlmerDevfun(devfun_lme4, reTrms, nAGQ = 1)
   if(k == 0L) {
     return(devfun_lme4)
   } else {
     if(requireNamespace("rgraphpass", quietly = TRUE)) {
       modfr <- list(fr = fr, X = X, reTrms = reTrms, family = family)
-      n_re <- nrow(reTrms$Zt)
+      n_fixed <- ncol(X)
       factorization_terms <- find_factorization_terms(modfr)
-      calibration_pars <- rgraphpass_parameters()
+      calibration_pars <- rgraphpass::calibration_parameters()
       calibration_pars$n_quadrature_points <- nAGQ
       calibration_pars$n_sparse_levels <- k
       calibration_pars$family <- family$family
       calibration_pars$link <- family$link
       tree <- rgraphpass::cluster_tree(factorization_terms)
-      belief_init <- rgraphpass::beliefs(factorization_terms)
+      beliefs_init <- rgraphpass::beliefs(factorization_terms, tree)
       devfun <- function(pars) {
-        beliefs <- belief_init
-        calibration_pars$theta <- pars[1:n_re]
-        calibration_pars$beta <- pars[-(1:n_re)]
+        beliefs <- beliefs_init
+        theta_size <- length(pars) - n_fixed
+        calibration_pars$theta <- pars[1:theta_size]
+        calibration_pars$beta <- pars[-(1:theta_size)]
         normal_approx <- compute_normal_approx(pars, devfun_lme4)
         beliefs$set_normal_approx(normal_approx$mean, normal_approx$precision)
         beliefs$calibrate(calibration_pars, tree, TRUE)
-        beliefs$calibrateForward(calibration_pars, tree, FALSE)
-        return(-2 * beliefs$log_normalizing_constant())
+        beliefs$calibrate_forward(calibration_pars, tree, FALSE)
+        return(-2 * beliefs$log_normalizing_constant(calibration_pars, tree))
       }
       return(devfun)
     } else {
       stop("you must install rgraphpass to use k > 0", call. = FALSE)
     }
   }
+}
+
+compute_normal_approx <- function(pars, devfun_lme4)
+{
+  devfun_lme4(pars)
+  PR <- get("pp", environment(devfun_lme4))
+  L_tot <- Matrix::expand(PR$L())
+  L <- L_tot$L
+  P <- L_tot$P
+  precision <- Matrix::t(P)%*%Matrix::tcrossprod(L)%*%P
+  mean <- PR$delu
+  return(list(mean = mean, precision = precision))
 }
