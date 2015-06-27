58e87e52dfb742e8898eb26ee916635cffcc142d
Helen Ogden <heogden12@gmail.com>
2015-06-27 16:33:26 +0100

    fix number of parameters in two-level model

--- inst/examples/two_level.R
+++ inst/examples/two_level.R
@@ -1,6 +1,6 @@
 set.seed(1)
 
-n_clusters <- 50
+n_clusters <- 5
 n_in_cluster <- 2
 n <- n_clusters*n_in_cluster
 cluster <- rep(1:n_clusters, each = n_in_cluster)
@@ -12,17 +12,17 @@ beta0 <- -0.5
 u0 <- rnorm(n_clusters)
 eta <- beta0 * x + sigma0 * u0[cluster]
 p <- exp(eta) / (1 + exp(eta))
-y <- rbinom(p)
+y <- rbinom(p, 1, p)
 
-data = list(y = y, x = x, cluster = cluster)
+two_level = list(y = y, x = x, cluster = cluster)
 
-devfun_approx <- glmerSR(y ~ x + (1 | cluster), data = data, family = binomial,
-                         nAGQ = 0, k = 0)
-devfun_10_lme4 <- glmerSR(y ~ x + (1 | cluster), data = data, family = binomial,
-                          nAGQ = 10, k = 0)
-devfun_10_sr <- glmerSR(y ~ x + (1 | cluster), data = data, family = binomial,
-                        nAGQ = 10, k = 0)
+devfun_approx <- lme4::glmer(y ~ x + (1 | cluster), data = two_level, family = binomial,
+                         nAGQ = 1, devFunOnly = TRUE)
+devfun_10_lme4 <- lme4::glmer(y ~ x + (1 | cluster), data = two_level, family = binomial,
+                          nAGQ = 10, devFunOnly = TRUE)
+devfun_10_sr <- glmerSR(y ~ x + (1 | cluster), data = two_level, family = binomial,
+                        nAGQ = 10, k = 1, devFunOnly = TRUE)
 
-devfun_approx(c(1, -0.5))
-devfun_10_lme4(c(1, -0.5))
-devfun_10_sr(c(1, -0.5))
+devfun_approx(c(1, 0, 1))
+devfun_10_lme4(c(1, 0, 1))
+devfun_10_sr(c(1, 0, 1))
