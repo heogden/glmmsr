a963b5776de6f9d89c39c8034308aee06754d953
Helen Ogden <heogden12@gmail.com>
2016-02-16 16:17:43 +0000

    test can compute loglikelihood (three-level)

--- tests/testthat/test_loglikelihood.R
+++ tests/testthat/test_loglikelihood.R
@@ -11,3 +11,10 @@ test_that("error for wrong length parameters", {
   expect_error(lfun_SR(rep(0.1, 4)),
                "cannot compute loglikelihood for parameter of length 4 != 3")
 })
+
+test_that("can compute SR log-likelihood for three-level model", {
+  modfr <- find_modfr_glmm(response ~ covariate + (1 | cluster) + (1 | group),
+                           data = three_level, family = binomial)
+  lfun_SR <- find_lfun_glmm(modfr, method = "SR")
+  lfun_SR(c(0.5, 0.5, 0 , 0))
+})
