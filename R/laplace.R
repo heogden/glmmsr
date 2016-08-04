4cedfbf8e262da87efb40b7d6f6d67a3340e796f
Helen Ogden <heogden12@gmail.com>
2016-08-04 08:26:32 +0100

    make separate fun: check HOL available

--- R/laplace.R
+++ R/laplace.R
@@ -1,10 +1,14 @@
+check_if_order_implemented <- function(family, order) {
+  stop("higher-order Laplace approximation not yet implemented",
+       call. = FALSE)
+}
+
 find_lfun_Laplace <- function(modfr, devfun_laplace_1, order) {
   if(order == 1L ) {
     lfun <- function(x) { -devfun_laplace_1(x) / 2 }
   }
   else {
-    stop("higher-order Laplace approximation not yet implemented",
-         call. = FALSE)
+    check_if_order_implemented(modfr$family, order)
   }
   lfun
 }
