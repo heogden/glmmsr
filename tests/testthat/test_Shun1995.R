6559545d6d7975deb805b72ad6168e1bdb7ab458
Helen Ogden <heogden12@gmail.com>
2016-10-05 09:58:42 +0100

    add extra tests for Shun1995

--- tests/testthat/test_Shun1995.R
+++ tests/testthat/test_Shun1995.R
@@ -22,12 +22,24 @@ modfr <- find_modfr_glmm(response ~ (1 | row_id) + (1 | col_id),
 lfun_Laplace_1 <- find_lfun_glmm(modfr, method = "Laplace")
 lfun_Laplace_2 <- find_lfun_glmm(modfr, method = "Laplace", control = list("order" = 2))
 
-
-pars <- c(1, 1, 1)
-lfun_Laplace_1_pars <- lfun_Laplace_1(pars)
-lfun_Laplace_2_pars <- lfun_Laplace_2(pars)
-
 test_that("second-order Laplace matches Shun1995", {
+  pars <- c(1, 1, 1)
+  lfun_Laplace_1_pars <- lfun_Laplace_1(pars)
+  lfun_Laplace_2_pars <- lfun_Laplace_2(pars)
   expect_true(abs(-72.6796 - lfun_Laplace_1_pars) < 1e-4)
   expect_true(abs(-72.2612 - lfun_Laplace_2_pars) < 1e-4)
+
+  pars_0pt5 <- c(0.5, 0.5, 1)
+  lfun_Laplace_1_pars_0pt5 <- lfun_Laplace_1(pars_0pt5)
+  lfun_Laplace_2_pars_0pt5 <- lfun_Laplace_2(pars_0pt5)
+  expect_true(abs(-70.0090 - 0.1024 - lfun_Laplace_1_pars_0pt5) < 1e-4)
+  expect_true(abs(-70.0090 + 0.0038 - lfun_Laplace_2_pars_0pt5) < 1e-4)
+
+  pars_3 <- c(3, 3, 1)
+  lfun_Laplace_1_pars_3 <- lfun_Laplace_1(pars_3)
+  lfun_Laplace_2_pars_3 <- lfun_Laplace_2(pars_3)
+  expect_true(abs(-85.6111 - 0.8525 - lfun_Laplace_1_pars_3) < 1e-4)
+  expect_true(abs(-85.6111 + 0.0547 - lfun_Laplace_2_pars_3) < 1e-4)
+
 })
+# (see Table 2, p 757, Shun1995)
